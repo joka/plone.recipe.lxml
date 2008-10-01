15abbb6a03dbf9e5c030f14ff3dfb5c7f4ef6762
zagy <zagy@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2008-10-01 08:45:30 +0000

    removed trailing whitespace, wrapped long lines

--- plone/recipe/lxml/__init__.py
+++ plone/recipe/lxml/__init__.py
@@ -7,58 +7,63 @@ WIN32 = False
 if sys.platform[:3].lower() == "win":
     WIN32 = True
 
+
 def system(c):
     if os.system(c):
         raise SystemError("Failed", c)
- 
+
+
 class Recipe:
     def __init__(self, buildout, name, options):
         self.buildout = buildout
         self.name = name
         self.options = options
-        
-        self.libxml2_url = options.get('libxml2-url', 'http://xmlsoft.org/sources/libxml2-2.6.29.tar.gz')
-        self.libxslt_url = options.get('libxslt-url', 'http://xmlsoft.org/sources/libxslt-1.1.21.tar.gz')
-        
+
+        self.libxml2_url = options.get(
+            'libxml2-url', 'http://xmlsoft.org/sources/libxml2-2.6.29.tar.gz')
+        self.libxslt_url = options.get(
+            'libxslt-url', 'http://xmlsoft.org/sources/libxslt-1.1.21.tar.gz')
+
         options['location'] = os.path.join(
             buildout['buildout']['parts-directory'],
             self.name
             )
-        location = options['location'] 
+        location = options['location']
 
         options.setdefault('include-dirs', 'lxml')
-        options.setdefault('egg', 'lxml == 1.3.3') 
+        options.setdefault('egg', 'lxml == 1.3.3')
         options.setdefault('include-dirs', location + '/include')
         options.setdefault('rpath', location + '/lib')
 
         self.egg = zc.recipe.egg.Custom(buildout, name, options)
-         
 
     def install(self):
         options = self.options
-        location = options['location']    
-        
+        location = options['location']
+
         if not os.path.exists(location):
             os.mkdir(location)
-        
+
         here = os.getcwd()
-        
+
         old_path = os.getenv('PATH')
         try:
             os.chdir(location)
-            
+
             # Build libxml2 and libxslt, unless buildout.cfg specifically
             # set those to a false value
-            
+
             if not WIN32 and self.libxml2_url:
                 self.cmmi(self.libxml2_url, '--without-python', location)
-            
+
             if not WIN32 and self.libxslt_url:
-                 self.cmmi(self.libxslt_url, '--without-python --with-libxml-prefix=%s' % location, location)
-            
+                 self.cmmi(
+                     self.libxslt_url,
+                     '--without-python --with-libxml-prefix=%s' % location,
+                     location)
+
             os.environ['PATH'] = location + '/bin:' + old_path
             self.egg.install()
-
 
         finally:
             os.environ['PATH'] = old_path
@@ -68,13 +73,11 @@ class Recipe:
 
     def update(self):
         pass
- 
-         
 
     def cmmi(self, url, extra_options, location):
-        
+
         # Code largely borrowed from zc.recipe.cmmi
-        
+
         _, _, urlpath, _, _, _ = urlparse.urlparse(url)
         tmp = tempfile.mkdtemp('buildout-'+self.name)
         tmp2 = tempfile.mkdtemp('buildout-'+self.name)
@@ -82,9 +85,9 @@ class Recipe:
             fname = os.path.join(tmp2, urlpath.split('/')[-1])
             open(fname, 'w').write(urllib2.urlopen(url).read())
             setuptools.archive_util.unpack_archive(fname, tmp)
-            
+
             here = os.getcwd()
-            os.chdir(tmp)                                        
+            os.chdir(tmp)
             try:
                 if not os.path.exists('configure'):
                     entries = os.listdir(tmp)
@@ -92,18 +95,14 @@ class Recipe:
                         os.chdir(entries[0])
                     else:
                         raise ValueError("Couldn't find configure")
-                
+
                 system("sh ./configure --prefix=%s %s" % (location, extra_options))
                 system("make")
                 system("make install")
-                
+
             finally:
                 os.chdir(here)
 
         finally:
             shutil.rmtree(tmp)
             shutil.rmtree(tmp2)
-            
-   
-   
- 
